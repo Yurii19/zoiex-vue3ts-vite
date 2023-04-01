b1cbf3a48d2cf397bdc353cdc7bfcbb7e1989ab6
Yurii19 <yurikolechkin@gmail.com>
2023-04-01 18:40:41 +0300

    chore: added logo and search ui elements

--- src/components/PrimaryApp.vue
+++ src/components/PrimaryApp.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="d-flex">
-  <v-img
-    width="100"
-    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_T835X5Z6ADme_fYEWOO03j7DfEjJoRjVwUtvMo8t5w&s"
-  ></v-img>
-  <v-text-field
-        hide-details
-        prepend-icon="mdi-magnify"
-        single-line
-      ></v-text-field>
-    </div>
+  <v-container>
+    <v-row no-gutters>
+      <v-col cols="3">
+        <v-sheet class="pa-2">
+          <v-img
+            class=""
+            width="100"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_T835X5Z6ADme_fYEWOO03j7DfEjJoRjVwUtvMo8t5w&s"
+          ></v-img>
+        </v-sheet>
+      </v-col>
+      <v-col cols="6">
+        <v-sheet class="pa-2">
+          <v-text-field
+            hide-details
+            prepend-icon="mdi-magnify"
+            single-line
+          ></v-text-field>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
   <iframe
     src="https://giphy.com/embed/dce5V6huS7QwhI3Xn6"
     width="343"
