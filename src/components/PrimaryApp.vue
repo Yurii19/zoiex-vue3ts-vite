f4548c33b8f42e501792fabb3da66d9d67cbd085
Yurii19 <yurikolechkin@gmail.com>
2023-04-02 19:28:06 +0300

    chore: add feature for sharing gif url

--- src/components/PrimaryApp.vue
+++ src/components/PrimaryApp.vue
@@ -21,11 +21,22 @@
   <div class="d-flex">
     <v-img
       v-for="gif in gifs"
-      :key="gif"
+      :key="gif.url"
       width="200"
       height="200"
-      :src="gif"
-    ></v-img>
+      :src="gif.url"
+      @click="shareGif"
+    >
+      <!-- <v-icon
+        class="share"
+        icon="mdi-share"
+        :data-title="gif.title"
+        :data-url="gif.url"
+        @click="shareGif"
+      ></v-icon
+    > -->
+      <i class="share" :data-title="gif.title" :data-url="gif.url"></i>
+    </v-img>
   </div>
   <p>
     <a href="https://giphy.com/gifs">via GIPHY</a>
@@ -41,11 +52,11 @@
 
 <script lang="ts" setup>
 import { onMounted, ref, watch } from 'vue';
-import { logo, API_KEY } from '../variables.js';
+import { logo, API_KEY, IGif } from '../variables.js';
 
 const endPoint: string = 'https://api.giphy.com/v1/gifs/search';
 
-const gifs = ref(['']);
+let gifs: any = ref([{ title: 'gif title ', url: 'gif url' }]);
 const inputValue = ref('');
 var timeoutId: any;
 const currentPage: any = ref(1);
@@ -55,12 +66,12 @@ const numberOfPages: any = ref(1);
 watch(inputValue, (newVal: string) => {
   clearTimeout(timeoutId);
   timeoutId = setTimeout(() => {
-    currentPage.value = 1
+    currentPage.value = 1;
     getGifs(newVal);
   }, 1000);
 });
 
-watch(gifs, (newVal: string[]) => {
+watch(gifs, (newVal: IGif[]) => {
   if (!newVal.length) {
     getGifs('not found', 1);
     // setTimeout(() => {
@@ -70,7 +81,7 @@ watch(gifs, (newVal: string[]) => {
 });
 
 watch(currentPage, (newVal: number) => {
-  if(newVal === 1) return
+  if (newVal === 1) return;
   const offset: number = (newVal - 1) * 5;
   getGifs(inputValue.value, 5, offset);
   console.log('current page is: ', newVal);
@@ -80,16 +91,29 @@ onMounted(() => {
   getGifs('random');
 });
 
+function shareGif(ev: any) {
+  const eventTargChild = ev.target.querySelector('.share');
+  console.log('evtTargChild ', eventTargChild);
+  navigator.share({
+    title: 'Gif',
+    text: eventTargChild.dataset.title,
+    url: eventTargChild.dataset.url,
+  });
+  //console.log('share', ev.target);
+}
+
 function getGifs(q: string, limit: number = 5, offset: number = 0) {
   //currentPage.value = 1;
   fetch(`${endPoint}?api_key=${API_KEY}&q=${q}&limit=${limit}&offset=${offset}`)
     .then((response) => {
-      // console.log(d);
       return response.json();
     })
     .then((response) => {
       console.log(response);
-      const gifUrls = response.data.map((el: any) => el.images.original.url);
+      const gifUrls: IGif[] = response.data.map((el: any) => ({
+        title: el.title,
+        url: el.images.original.url,
+      }));
       gifs.value = gifUrls;
       const amount = Math.floor(response.pagination.total_count / 5);
       const remainder = response.pagination.total_count / 5;
@@ -109,4 +133,13 @@ function getGifs(q: string, limit: number = 5, offset: number = 0) {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.share {
+  /* background-color: black;
+  color: white;
+  border-radius: 50%;
+  padding: 15px;
+  cursor: pointer;
+  border: 2px solid white; */
+}
+</style>
